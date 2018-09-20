b78cfbf3ef86b90baeadea2bf5528800aa7b4afc
pawellaskowski <pawel.jan.laskowski@gmail.com>
2018-09-20 13:01:19 +0200

    Move LayoutManager declaration to XML

--- app/src/main/java/com/pjl/kyubee/history/HistoryFragment.kt
+++ app/src/main/java/com/pjl/kyubee/history/HistoryFragment.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,7 +20,6 @@ class HistoryFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_history, container, false)
         view.solve_list.adapter = adapter
-        view.solve_list.layoutManager = LinearLayoutManager(activity!!)
         return view
     }
 
